ae6c9ed79c6b9c12a22eff5bfaa5298355a79253
Antonio Ribeiro <antonio_rmn@hotmail.com>
2025-06-16 13:44:28 -0400

    Adicionando o resultado no console

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else {
         listasDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listasDeNumerosSorteados);
         return numeroEscolhido;
     }
 }
